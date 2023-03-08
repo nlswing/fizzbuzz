565911eacf8eed73b0b8458ce20db5de1dff8ddd
Nathan Wing <wing_nathan@hotmail.com>
2023-03-08 11:09:21 +0000

    "Third passing test"
    Co-authored-by: Jon Pillay <jonpillay@users.noreply.github.com>

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,12 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
+  return "FizzBuzz"
+  elsif number % 3 == 0
     return "fizz"
   elsif number % 5 == 0
     return "buzz"
+  else 
+    return number
   end
 end
+
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,10 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "FizzBuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+  it 'returns the number when passed any other number' do
+    expect(fizzbuzz(2)).to eq 2
+  end
 end
